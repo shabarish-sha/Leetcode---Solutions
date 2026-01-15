47435d8765d9f0e10f35cebc929682ea9ca3d47a
Shabarish <shabarish120608@gmail.com>
2026-01-15 22:29:16 +0530

    Add valid parentheses

---
+++ C/Valid parentheses - 20.c
@@ -0,0 +1,19 @@
+bool isValid(char* s) {
+    char stack[10000];
+    int top = -1;
+
+    for (int i = 0; s[i] != '\0'; i++) {
+        char c = s[i];
+        if (c == '(' || c == '{' || c == '[') {
+            stack[++top] = c;
+        } else {
+            if (top < 0) return false;
+            char t = stack[top--];
+            if ((c == ')' && t != '(') ||
+                (c == '}' && t != '{') ||
+                (c == ']' && t != '['))
+                return false;
+        }
+    }
+    return top == -1;
+}
