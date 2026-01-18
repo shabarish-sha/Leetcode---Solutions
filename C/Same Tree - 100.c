1220dfddfd174e7bc246bf91d7408af57db1e736
Shabarish <shabarish120608@gmail.com>
2026-01-18 14:51:46 +0530

    Implement isSameTree function for binary trees

---
+++ C/Same Tree - 100.c
@@ -0,0 +1,5 @@
+bool isSameTree(struct TreeNode* p, struct TreeNode* q) {
+    if (!p && !q) return true;
+    if (!p || !q || p->val != q->val) return false;
+    return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+}
